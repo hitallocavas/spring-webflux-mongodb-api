149e228749fa41171b1223c3d7b5ce16db639d58
hitallocavas <hcs3@cin.ufpe.br>
2020-12-13 18:18:14 -0300

    Ajustes nas propriedades

--- src/main/java/br/com/reactive/webflux/api/entrypoints/controller/PlaylistController.java
+++ src/main/java/br/com/reactive/webflux/api/entrypoints/controller/PlaylistController.java
@@ -5,13 +5,19 @@ import br.com.reactive.webflux.api.domain.parser.PlaylistParser;
 import br.com.reactive.webflux.api.entrypoints.request.PlaylistRequest;
 import br.com.reactive.webflux.api.service.PlaylistService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+
+import java.time.Duration;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "playlists")
+@Slf4j
 public class PlaylistController {
 
     private final PlaylistService playlistService;
@@ -29,5 +35,13 @@ public class PlaylistController {
     @PostMapping
     public Mono<Playlist> save(@RequestBody PlaylistRequest playlistRequest){
         return playlistService.save(PlaylistParser.parseRequest(playlistRequest));
+    }
+
+    @GetMapping(value = "events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Tuple2<Long, Playlist>> findEvents(){
+        Flux<Long> interval = Flux.interval(Duration.ofSeconds(5L));
+        Flux<Playlist> events = playlistService.findAll();
+        log.info("Passou por evento");
+        return Flux.zip(interval, events);
     }
 }
