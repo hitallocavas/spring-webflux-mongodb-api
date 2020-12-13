c79cf2403fb686c38d3c9dfd99670d0d76a930ef
hitallocavas <hcs3@cin.ufpe.br>
2020-12-13 12:58:34 -0300

    Crud de Playlists

--- src/main/java/br/com/reactive/webflux/api/domain/document/Playlist.java
+++ src/main/java/br/com/reactive/webflux/api/domain/document/Playlist.java
@@ -3,15 +3,15 @@ package br.com.reactive.webflux.api.domain.document;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.MongoId;
 
 @Getter
 @Setter
 @Builder
 @Document
 public class Playlist {
-    @MongoId
+    @Id
     private String id;
     private String name;
 }
--- src/main/java/br/com/reactive/webflux/api/repository/PlaylistRepository.java
+++ src/main/java/br/com/reactive/webflux/api/repository/PlaylistRepository.java
@@ -3,6 +3,4 @@ package br.com.reactive.webflux.api.repository;
 import br.com.reactive.webflux.api.domain.document.Playlist;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
-public interface PlaylistRepository extends ReactiveMongoRepository<Playlist, String> {
-
-}
+public interface PlaylistRepository extends ReactiveMongoRepository<Playlist, String> { }
